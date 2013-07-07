f4cd288b794513b75ca3c5b231c688bc35695129
DrJByrne <dr.j.byrne@gmail.com>
2013-07-07 13:38:26 +0000

    2nd-guess edit of web.js

--- web.js
+++ web.js
@@ -1,18 +1,10 @@
-Var express = require('express');
+var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html', 'utf8', function (err, data) {
-      if (err) throw err; 
-      console.log(data);
-  }
-));
+    response.send(fs.readFileSync('index.html', 'utf8'));
 });
-
-#
-#
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
