9a3d4c6fdc6ed4433e9aca158456917c80a3c348
DrJByrne <dr.j.byrne@gmail.com>
2013-07-07 14:51:47 +0000

    web.js

--- web.js
+++ web.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-    response.send(fs.readFileSync('index.html', 'utf8'));
+app.get('/', function(request, response) { 
+    var fs = require('fs');
+    var buffer = new Buffer(256);
+    response.send(buffer.toString(fs.readFileSync('index.html','utf8')));
 });
 
 var port = process.env.PORT || 5000;
