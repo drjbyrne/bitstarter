d06d48da568ce63fa38d0b2bd8f0c98a79348500
DrJByrne <dr.j.byrne@gmail.com>
2013-07-07 12:30:47 +0000

    Edited web.js

--- web.js
+++ web.js
@@ -1,10 +1,18 @@
-var express = require('express');
+Var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fs.readFileSync('index.html', 'utf8', function (err, data) {
+      if (err) throw err; 
+      console.log(data);
+  }
+));
 });
+
+#
+#
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
